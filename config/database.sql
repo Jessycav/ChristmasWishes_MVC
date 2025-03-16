016b1743cd400970593023890dd6155b2705e039
Jessycav <jessyca.vannier@gmail.com>
2025-03-16 19:57:55 +0100

    update config

--- config/database.sql
+++ config/database.sql
@@ -8,8 +8,6 @@ CREATE TABLE `gift` (
   FOREIGN KEY (`wishlist_id`) REFERENCES `wishlist` (`wishlist_id`) ON DELETE CASCADE
 ) 
 
-INSERT INTO `gift` VALUES (117,'DOMINO MUSIQUE','DÃ©couvrez les animaux de la jungle et les instruments de musique. ComposÃ© de 28 piÃ¨ces, ce jeu de dominos est aussi bien ludique qu&#039;Ã©ducatif. ','https://www.joueclub.fr/jeux-educatifs/domino-musique-3700217326234.html','https://joueclub-joueclub-fr-storage.omn.proximis.com/Imagestorage/imagesSynchro/0/0/fb00ebacffa75621fc50498f4c0535be3ce2381a_41138581.jpeg',30),(118,'ENSEMBLE DE 5 POCHOIRS MEGASKETCHER','Set de pochoirs marins pour prolonger l&amp;amp;#039;expÃ©rience de dessin avec votre ardoise magique Megasketcher','https://www.joueclub.fr/activites-creatives-et-manuelles/ensemble-de-5-pochoirs-megasketcher-5011666735828.html','https://joueclub-joueclub-fr-storage.omn.proximis.com/Imagestorage/imagesSynchro/0/0/89527193ec40f0854d4213e038b9c0b466f3dc96_41127885.jpeg',3),(119,'DOMINO MUSIQUE','DÃ©couvrez les animaux de la jungle et les instruments de musique. ComposÃ© de 28 piÃ¨ces, ce jeu de dominos est aussi bien ludique qu&#039;Ã©ducatif. ','https://www.joueclub.fr/jeux-educatifs/domino-musique-3700217326234.html','https://joueclub-joueclub-fr-storage.omn.proximis.com/Imagestorage/imagesSynchro/0/0/fb00ebacffa75621fc50498f4c0535be3ce2381a_41138581.jpeg',3);
-
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `user_firstname` varchar(150) DEFAULT NULL,
@@ -18,8 +16,6 @@ CREATE TABLE `user` (
   `password` varchar(255) DEFAULT NULL,
 ) 
 
-INSERT INTO `user` VALUES (5,'Jessyca','jess','miss_anandhi@hotmail.fr','$2y$10$krpWqPYghhBXPhbTv.DHbev1Flfih9jEZbBnGMmlMridiZxVRmNwS');
-
 CREATE TABLE `wishlist` (
   `wishlist_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `wishlist_year` int(4) NOT NULL,
@@ -27,5 +23,3 @@ CREATE TABLE `wishlist` (
   `user_id` int(11) DEFAULT NULL,
   FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE
 ) 
-
-INSERT INTO `wishlist` VALUES (3,2024,'Ileana',5),(30,2024,'Chase',5);
