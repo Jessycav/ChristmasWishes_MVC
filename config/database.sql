ee98c520b8ddf14fa8ba806f56367ca8c31eacb4
Jessycav <jessyca.vannier@gmail.com>
2025-03-16 21:26:17 +0100

    update config

--- config/database.sql
+++ config/database.sql
@@ -1,13 +1,3 @@
-CREATE TABLE `gift` (
-  `gift_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `gift_title` VARCHAR(255) NOT NULL,
-  `gift_description` TEXT DEFAULT NULL,
-  `gift_link` VARCHAR(255) DEFAULT NULL,
-  `gift_image` VARCHAR(255) DEFAULT NULL,
-  `wishlist_id` INT(11) DEFAULT NULL,
-  FOREIGN KEY (`wishlist_id`) REFERENCES `wishlist` (`wishlist_id`) ON DELETE CASCADE
-) ENGINE=InnoDB CHARSET=utf8;
-
 CREATE TABLE `user` (
   `user_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `user_firstname` VARCHAR(150) DEFAULT NULL,
@@ -23,4 +13,14 @@ CREATE TABLE `wishlist` (
   `wishlist_recipient` VARCHAR(150) NOT NULL,
   `user_id` INT(11) DEFAULT NULL,
   FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB CHARSET=utf8;
+
+CREATE TABLE `gift` (
+  `gift_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `gift_title` VARCHAR(255) NOT NULL,
+  `gift_description` TEXT DEFAULT NULL,
+  `gift_link` VARCHAR(255) DEFAULT NULL,
+  `gift_image` VARCHAR(255) DEFAULT NULL,
+  `wishlist_id` INT(11) DEFAULT NULL,
+  FOREIGN KEY (`wishlist_id`) REFERENCES `wishlist` (`wishlist_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB CHARSET=utf8;
