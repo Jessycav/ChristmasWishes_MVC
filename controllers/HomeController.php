ae86b83b1d541d1cea1df28800d40aec5a27146b
Jessycav <jessyca.vannier@gmail.com>
2025-03-17 12:48:44 +0100

    delete test_db

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -3,7 +3,7 @@
 class HomeController { //Création de la classe
     //Création de la méthode homePage
     public function homePage() { 
-        require_once __DIR__ . '/../views/pages/test_db.php';
+        require_once __DIR__ . '/../views/pages/homePage.php';
     }
 }
 ?>
