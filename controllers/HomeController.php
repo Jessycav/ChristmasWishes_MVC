59c0e2a26daf5dceaa462be61d72dc80a7f3f74b
Jessycav <158269752+Jessycav@users.noreply.github.com>
2025-03-17 14:23:39 +0100

    Update HomeController.php

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -3,7 +3,7 @@
 class HomeController { //Création de la classe
     //Création de la méthode homePage
     public function homePage() { 
-        require_once __DIR__ . '/../views/pages/homePage.php';
+        require_once __DIR__ . '/../test_db.php';
     }
 }
 ?>
